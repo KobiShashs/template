6bc1fa74709419f30695c285f406aa85073f59b1
Kobi <kobi.shasha@gmail.com>
2020-05-01 17:38:54 +0300

    Initial commit

---
+++ src/test/java/com/ttt/il/template/TemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttt.il.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
